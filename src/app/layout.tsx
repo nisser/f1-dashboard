9ed9244909ca5d48a5e829c8b0ac8928b8bd5c8e
Matas Jovaiša <jovaisamatas@gmail.com>
2025-07-04 23:05:31 +0300

    Font update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Orbitron } from "next/font/google";
 import "./globals.css";
 import "leaflet/dist/leaflet.css";
 import TeamThemeSwitcher from "@/components/TeamThemeSwitcher";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const orbitron = Orbitron({
+  variable: "--font-orbitron",
   subsets: ["latin"],
 });
 
@@ -26,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${orbitron.variable} antialiased`}>
         <header className="sticky top-0 z-50 bg-slate-900/70 backdrop-blur-[3px] shadow-md">
           <div className="flex items-center justify-between px-4 py-2">
             <h1 className="text-xl font-bold text-white">F1 Dashboard</h1>
