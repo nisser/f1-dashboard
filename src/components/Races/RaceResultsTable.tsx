16039c368ab08c1c0de6dd2207bb805c15c0abd1
Matas Jovaiša <jovaisamatas@gmail.com>
2025-07-16 23:45:29 +0300

    Refined result table

--- src/components/Races/RaceResultsTable.tsx
+++ src/components/Races/RaceResultsTable.tsx
@@ -5,29 +5,39 @@ import { RaceWithResults } from '@/lib/types'
 
 export default function RaceResultsTable({ results }: { results: RaceWithResults }) {
   return (
-    <table className="min-w-full border-collapse border border-gray-700 rounded-lg overflow-hidden text-sm">
-      <thead className="bg-gray-800 text-gray-200">
-        <tr>
-          <th className="border border-gray-600 px-3 py-1 text-left">Pos</th>
-          <th className="border border-gray-600 px-3 py-1 text-left">Driver</th>
-          <th className="border border-gray-600 px-3 py-1 text-left">Team</th>
-          <th className="border border-gray-600 px-3 py-1 text-left">Time</th>
-          <th className="border border-gray-600 px-3 py-1 text-left">Points</th>
-        </tr>
-      </thead>
-      <tbody className="bg-black/25 text-gray-300">
-        {results.Results?.map((result) => (
-          <tr key={result.position} className="hover:bg-gray-700 transition">
-            <td className="border border-gray-600 px-3 py-1">{result.position}</td>
-            <td className="border border-gray-600 px-3 py-1">
-              {result.Driver.givenName} {result.Driver.familyName}
-            </td>
-            <td className="border border-gray-600 px-3 py-1">{result.Constructor.name}</td>
-            <td className="border border-gray-600 px-3 py-1">{result.Time?.time || '-'}</td>
-            <td className="border border-gray-600 px-3 py-1">{result.points}</td>
+    <div onClick={(e) => e.stopPropagation()}>
+      <table className="min-w-full rounded-lg overflow-hidden text-xs">
+        <thead className="bg-black/50 text-gray-200">
+          <tr>
+            <th className="px-2 py-1">#</th>
+            <th className="px-2 py-1">Driver</th>
+            <th className="px-2 py-1">Team</th>
+            <th className="px-2 py-1">Time</th>
+            <th className="px-2 py-1">Pts.</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className="bg-black/25 text-gray-300">
+          {results.Results?.map((result) => (
+            <tr key={result.position} className="hover:bg-black/20 transition">
+              <td className="px-2 py-1">{result.position}</td>
+              <td className="px-2 py-1">{result.Driver.code}</td>
+              <td className="px-2 py-1">{result.Constructor.name}</td>
+              <td className="px-2 py-1">
+                {result.status === 'Lapped'
+                  ? '+1 Lap'
+                  : result.status === 'Retired'
+                    ? 'DNF'
+                    : result.status === 'Did not start'
+                      ? 'DNS'
+                        : result.status === 'Disqualified'
+                        ? 'DSQ'
+                        : result.Time?.time || '-'}
+              </td>
+              <td className="px-2 py-1">{result.points}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
